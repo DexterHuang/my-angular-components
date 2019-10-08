e1e4b3a50a14acc7a9713d688519abf68e35f429
Dexter <huang.ching@chubb.com>
2019-10-08 15:21:13 +0800

    1

--- src/app/Helper/CryptoHelper.ts
+++ src/app/Helper/CryptoHelper.ts
@@ -1,28 +1,20 @@
-import * as crypto from "crypto";
 import * as CryptoJS from "crypto-js";
 export class CryptoHelper {
-  // static encrypt(text: string, password: string) {
-  //   var cipher = crypto.createCipher("aes-256-ctr", password);
-  //   var crypted = cipher.update(text, "utf8", "hex");
-  //   crypted += cipher.final("hex");
-  //   return crypted;
-  // }
-  // static decrypt(text: string, password: string) {
-  //   var decipher = crypto.createDecipher("aes-256-ctr", password);
-  //   var dec = decipher.update(text, "hex", "utf8");
-  //   dec += decipher.final("utf8");
-  //   return dec;
-  // }
   static encrypt(text: string, password: string) {
-    const encrypted = CryptoJS.AES.encrypt(text, password);
+    const encrypted = CryptoJS.AES.encrypt(text, password).toString();
     return encrypted;
   }
   static decrypt(text: string, password: string) {
-    var decipher = crypto.createDecipher("aes-256-ctr", password);
-    var dec = decipher.update(text, "hex", "utf8");
-    dec += decipher.final("utf8");
-    return dec;
+    const decrypted = CryptoJS.AES.decrypt(text, password).toString(
+      CryptoJS.enc.Utf8
+    );
+    return decrypted;
+  }
+  static encryptObject(obj: Object, password: string) {
+    return this.encrypt(JSON.stringify(obj), password);
+  }
+  static decryptObject(text: string, password) {
+    const json = this.decrypt(text, password);
+    return JSON.parse(json);
   }
 }
-
-console.log("hi");
