8c1402df28c3c3a303e88e344b7319680c4517d9
DexterHuang <ching.huang.dexter@gmail.com>
2019-10-09 10:46:05 +0800

    fixed buffer

--- src/app/Helper/ThreadHandler.ts
+++ src/app/Helper/ThreadHandler.ts
@@ -1,22 +1,36 @@
 import { buffer } from "rxjs/operators";
 import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import { Observable, Subject } from "rxjs";
 export class ThreadHandler {
-  static bufferMap: { [key: string]: BehaviorSubject<any> } = {};
-  static buffer<T>(
+  static subjectMap: { [key: string]: Subject<any> } = {};
+  static buffer<T, R>(
     key: string,
     l: T[],
-    task: (list: T[]) => Promise<void>,
+    task: (list: T[]) => Promise<R>,
     { debounceTime = 500 } = {}
-  ): void {
-    let subject = this.bufferMap[key];
-    if (!subject) {
-      this.bufferMap[key] = new BehaviorSubject(null);
-      subject = this.bufferMap[key];
-      subject
-        .filter(i => !!i)
-        .pipe(buffer(subject.debounceTime(debounceTime)))
-        .subscribe(task);
-    }
-    l.forEach(i => subject.next(i));
+  ): Promise<R> {
+    return new Promise(resolve => {
+      const onCompleteKey = `${key}_ON_COMPLETE`;
+      let subject = this.subjectMap[key];
+      if (!subject) {
+        this.subjectMap[key] = new Subject();
+        subject = this.subjectMap[key];
+        const debounced = subject.pipe(
+          buffer(subject.debounceTime(debounceTime))
+        );
+
+        const onCompleteSubject = new Subject();
+        this.subjectMap[onCompleteKey] = onCompleteSubject;
+        debounced.subscribe(async list => {
+          const result = await task(list);
+          onCompleteSubject.next(result);
+        });
+      }
+      const onComplete = this.subjectMap[onCompleteKey];
+      onComplete.subscribe(result => {
+        resolve(result);
+      });
+      l.forEach(i => subject.next(i));
+    });
   }
 }
--- src/app/TestComponent/TestComponent.ts
+++ src/app/TestComponent/TestComponent.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import ObserverComponent from "../../ObserverComponent";
 import { MainStore } from "../Store/MainStore";
-import { CryptoHelper } from "../Helper/CryptoHelper";
+import { ThreadHandler } from "../Helper/ThreadHandler";
 @Component({
   selector: "app-test-component",
   templateUrl: "./app-test-component.html",
@@ -10,16 +10,29 @@ import { CryptoHelper } from "../Helper/CryptoHelper";
 export class TestComponent extends ObserverComponent implements OnInit {
   potato = [1, 1, 1, 1, 1];
   testValue;
-  ngOnInit() {
+  async ngOnInit() {
     this.observe(MainStore.testValue, v => (this.testValue = v));
     // setInterval(() => {
     //   MainStore.testValue.next(this.testValue + 1);
     // }, 1000);
+
+    console.log("start");
+    const task = (l: number[]) => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve(l);
+        }, 1000);
+      });
+    };
+    ThreadHandler.buffer("1", [1, 1, 1, 1], task);
+    ThreadHandler.buffer("1", [2, 2, 2, 2], task);
+    const r = await ThreadHandler.buffer("1", [1, 1, 11], task);
+    console.log({ r });
   }
   onClickSave() {}
   onClickLoad() {}
 }
 
-const encrypted = CryptoHelper.encryptObject({ ppap: 1 }, "pass");
-const decrypted = CryptoHelper.decryptObject(encrypted, "pass");
-console.log({ decrypted });
+// const encrypted = CryptoHelper.encryptObject({ ppap: 1 }, "pass");
+// const decrypted = CryptoHelper.decrypt(encrypted, "pass");
+// console.log({ decrypted });
